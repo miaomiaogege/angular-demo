5e5d4e28dc08c38a66c58bb3deca51f74e791885
chengzao <c1056660442@163.com>
2017-03-10 01:02:12 +0800

    'templateUrl'

--- compile-link/compile-link.js
+++ compile-link/compile-link.js
@@ -10,10 +10,20 @@ myApp.directive('repeater',function(){
 			for(var i=0;i<attrs.repeater-1;i++){
 				ele.append(tpl.clone());
 			}
-			// 返回 link 函数
+
+			// return {
+			// 	//当前子元素
+			// 	pre:function(){},
+			// 	// 所有子元素
+			// 	post:function(){}
+			// }
+
+			// 返回 post link 函数
 			return function(scope,ele,attrs,controller){
 				console.log('compile2..link..')
-			}
+			},
+			// post link..
+			link:function(){}
 		}
 	}
 })
--- directive-1/hello-2.js
+++ directive-1/hello-2.js
@@ -6,3 +6,12 @@ myApp.directive('hello',function(){
 		replace:true
 	}
 })
+
+
+// templateUrl  template :
+// 模版中需要有html标签包裹 ： <div>haha...</div>
+
+// templateUrl:'tmpl',
+// <script type='text/ng-template' id='tmpl'>
+//	  <div>haha...</div>
+// </script>	
